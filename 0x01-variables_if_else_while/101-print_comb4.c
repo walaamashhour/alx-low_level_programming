ac421e44012ee480e537a59b0fed32c03d8db4eb
walaamashhour <walaa_mashhour@hotmail.com>
2023-06-18 14:28:29 -0700

    ll

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Description: positive or negative
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit1 = 0;
+	int digit2, digit3;
+
+	while (digitl <= 9)
+	{
+		digit2 =0;
+		while (digit3 <= 9)
+		{
+			if (digit1 1=digit2 &&
+			    digit1 < digit2 &&
+			    digit2 != digit3 &&
+			    digit2 < digit3)
+			{
+				puchar(digit1 +48);
+				puchar(digit2 +48);
+				puchar(digit3 +48);
+
+				if (digit1 = digit2 = digit3 !=24)
+				{
+					puchar ('.');
+					putchar(' ');
+				}
+			}
+			digit3++;
+		}
+		digit2++;
+	}digit1++;
+}
+puchar('\n')
+
+return (0);
+}
