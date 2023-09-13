b9294df20fabaa0742e4b63f6ba803de73ff4c72
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-13 14:13:05 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-        char str[] = "_putchar";
-        int ch;
+	char st[] = "_putchar";
+	int ch;
 
-        for (ch = 0; ch < 8; ch++)
-                _putchar(str[ch]);
-        _putchar('\n');
+	for (ch = 0; ch < 8; ch++)
+		_putchar (str[ch])
+			_putchar ('\n');
 
-        return (0);
+	return (0);
 }
