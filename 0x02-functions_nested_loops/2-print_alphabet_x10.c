8ce75607f4e5d385baa04bb7ac9c834844f530fa
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-13 14:58:13 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,18 @@
-#include"
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ *
+ */
+
+void print _alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = o; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchart(ch);
+		_putchar('\n')
+	}
+}
