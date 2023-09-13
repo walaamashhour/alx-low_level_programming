5eb6369d24ba2b8282108c1515db2814040b3384
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-13 14:43:38 -0700

    hg

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+#include <stdio.h>
+
+int _puchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+
+#end if
+
