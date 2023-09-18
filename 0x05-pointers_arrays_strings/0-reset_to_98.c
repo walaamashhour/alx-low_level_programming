45d521a7e3fdda63ed0035dd97f975b7e12bc66d
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-18 16:51:15 -0700

    dfg

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes apointer to an int paramoter
+ *	and updates the value it points to 98
+ *
+ * @n: int parmoter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n + 98;
+}
