61cb1aba864a5654e47b759c57406d68331f4194
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-18 17:35:41 -0700

    df

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,19 +2,18 @@
 
 /**
  * swap_int - swaps the values of two integers
- * using two input parameters
  *
- * @a: input parameter 1
- * @b: input parameter 2
+ * @a: integer to swap
+ * @b: integer to swap
  *
  * Return: Nothing
  */
 
 voide swap_int(int *a, int *b)
 {
-	int temp;
+	int m;
 
-	temp = *a;
+	m = *a;
 	*a = *b;
-	*b = temo;
+	*b = m;
 }
