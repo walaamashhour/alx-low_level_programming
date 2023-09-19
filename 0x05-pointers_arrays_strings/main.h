ccc8b3375108dbc65d52f5147d874d90139ea86d
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-18 17:22:39 -0700

    jj

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 int _puchar(char c);
 void reset_to_98(int *n);
@@ -16,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H*/
+#endif
