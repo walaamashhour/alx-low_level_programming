89a6de63175ea02d259061babdc41b1096743649
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-18 18:28:52 -0700

    gg

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,15 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  *  @str: string to print
+ *  Return: Nothing
  */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*str + 0);
+		str++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -8,7 +8,7 @@
 * on error, -1 is returned, and errno is set appropriately.
 */
 
-int _putchar(char c)
+int _putchar(char c);
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
