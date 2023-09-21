b082066af21174f76ee0bcecf52d0d380d91d8fc
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-21 16:29:27 -0700

    l

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -6,9 +6,11 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13)
+		is a simple letter substitution cipher.\n";
 	char *p;
 
 	p = rot13(s);
