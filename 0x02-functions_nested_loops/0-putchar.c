c42fb03ad606c0ebbe9b06dce2943810e5d158c0
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-23 15:02:23 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,20 @@
 /**
  * main - entry point
  *
- * Description: prints _putchar using putchar prototype
- *
- * Return: 0 (success)
-*/
+ * Return: Always 0
+ */
 
 int main(void)
 {
-	char st[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ch++)
-		_putchar (str[ch])
-			_putchar ('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
