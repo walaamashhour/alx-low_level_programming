c3fa8683c6456d6e18536597127c86bc68db6bc7
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-24 18:05:41 -0700

    k

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
- * main -entry point
+ * main - Entry point
  *
- * Description: print numbers of base16 in lowercase 
+ * Description: print numbers of base 16 in lowercase
  *
  * Return: 0 (success)
 */
 
 int main(void)
 {
-	int digit + 48; /*48; decimal rep of 0*/
+	int digit = 48;/*48; decimal rep of 0*/
+
 	while (digit <= 102) /*102; decimal rep of f*/
 	{
-		putchar(digit):
+		putchar(digit);
 
 		/*after 9 we jump till 96; `*/
 		if (digit == 57)
-			digit =+ 39;
+			digit += 39;
 		digit++;
 	}
 	putchar('\n');
