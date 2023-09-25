b3937b6f982c50ab6665debac4bc7ba137afaa72
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-25 15:11:05 -0700

    g

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - memory area pointed
+ * @s: string.
+ * @accept: bytes.
+ *
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+			return (i);
+		}
+	}
+return (i);
+}
