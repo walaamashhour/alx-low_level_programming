8e89a965bc468f530035c1257a9771ef472d5e5d
walaamashhour <walaa_mashhour@hotmail.com>
2023-09-25 15:24:26 -0700

    k

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string
+ * @s: string
+ * @accept: string to match
+ * Return: pointer to the byte in s or null if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
