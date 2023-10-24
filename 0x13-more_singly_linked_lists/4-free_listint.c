edaddeea188f8aef2fb0e4840b37440cf6e7dfbb
walaamashhour <walaa_mashhour@hotmail.com>
2023-10-24 12:39:43 -0700

    h

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to first node
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head->next;
+		node = head;
+		free(head);
+	}
+}
+
