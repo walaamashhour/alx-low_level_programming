536fa737c54a9d0821d39d79973d2795b929d831
walaamashhour <walaa_mashhour@hotmail.com>
2023-10-27 17:22:16 -0700

    ff

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int bit, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
+
